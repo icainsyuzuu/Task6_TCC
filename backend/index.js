70aee45a121142f326782b5ff3a55e3fc07a95f4
icainsyuzuu <ica.insyuzuu514@gmail.com>
2025-05-27 22:18:38 +0700

    up

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,11 @@ app.use(express.json());
 
 app.use("/api", NoteRoute);
 app.use("/api/auth", AuthRoute);
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, "127.0.0.1", () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "127.0.0.1", () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server berjalan di port ${PORT}`);
 });
