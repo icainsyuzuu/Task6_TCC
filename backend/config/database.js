1145714d698655aeea36d9cda5abb8788b95d9e2
icainsyuzuu <ica.insyuzuu514@gmail.com>
2025-05-29 17:46:41 +0700

    update cors v5

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,18 @@
-// import { Sequelize } from "sequelize";
-// const db = new Sequelize("note", "root", "cantik", {
-//   host: "34.46.192.156",
-//   dialect: "mysql",
-// });
-
-// export default db;
-
 import { Sequelize } from "sequelize";
-
-const db = new Sequelize("note", "root", "", {
-  host: "127.0.0.1", // pakai IPv4 explicit
+const db = new Sequelize("note", "root", "cantik", {
+  host: "34.46.192.156",
   dialect: "mysql",
-  port: 3307,
-  logging: false, // opsional supaya log lebih bersih
 });
 
 export default db;
+
+// import { Sequelize } from "sequelize";
+
+// const db = new Sequelize("note", "root", "", {
+//   host: "134.46.192.156", // pakai IPv4 explicit
+//   dialect: "mysql",
+//   port: 3306,
+//   logging: false, // opsional supaya log lebih bersih
+// });
+
+// export default db;
