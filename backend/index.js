24a0db252c58eb4045fde6bc0eabda53e76c3707
icainsyuzuu <ica.insyuzuu514@gmail.com>
2025-05-29 15:31:57 +0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,22 @@ import express from "express";
 import cors from "cors";
 import NoteRoute from "./routes/NoteRoute.js";
 import AuthRoute from "./routes/AuthRoute.js";
+
 const app = express();
 
-app.use(cors());
+// Konfigurasi CORS khusus
+app.use(
+  cors({
+    origin: "https://h-05-450908.uc.r.appspot.com",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api", NoteRoute);
 app.use("/api/auth", AuthRoute);
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, "127.0.0.1", () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
