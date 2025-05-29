213508b1adc4659e44d3cbfb16c6eafd3bc62e4b
icainsyuzuu <ica.insyuzuu514@gmail.com>
2025-05-29 15:26:14 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import NoteRoute from "./routes/NoteRoute.js";
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use("/api", NoteRoute);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
 import express from "express";
 import cors from "cors";
 import NoteRoute from "./routes/NoteRoute.js";
--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://be-1013759214686.us-central1.run.app/api";
-const BASE_URL = "http://localhost:5000/api"; 
+export const BASE_URL = "https://be-1013759214686.us-central1.run.app/api";
+// const BASE_URL = "http://localhost:5000/api"; 
 
 
 document.addEventListener("DOMContentLoaded", () => {
